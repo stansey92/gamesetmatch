7e3e52a1aeadc60e02ad9d429731e6a0c2d30d9f
andersontyler65 <andersontyler65@gmail.com>
2018-01-08 13:11:41 -0700

    redeploy

--- config/config.js
+++ config/config.js
@@ -1,1 +1,1 @@
-const dbURL = "https://gamesetmatch-db.herokuapp.com/"
+const dbURL = "postgres://koljinwbzfeyep:049963a8c5516dfd03cf8a397ee5c39c26682b78f302cb3fbc9e63647a6ec584@ec2-23-21-246-25.compute-1.amazonaws.com:5432/d7nnhvov7vn0kt"
